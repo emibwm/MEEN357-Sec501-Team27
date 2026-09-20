8a87fd6b57ec6a85490518a62d85eb87b97970a6
Emily  Bass Werner Maldonado <emilybwm@tamu.edu>
2026-09-20 13:26:37 -0500

    Rename function tau_motordc to tau_dcmotor

--- subfunctions.py
+++ subfunctions.py
@@ -159,7 +159,7 @@ def F_drive(omega, rover):
   if not isinstance(rover, dict):
         raise Exception('Invalid input: rover must be a dict')
   ratio = get_gear_ratio(rover['wheel_assembly']['speed_reducer'])
-  tau = tau_motordc(omega, rover) #get motor torque
+  tau = tau_dcmotor(omega, rover) #get motor torque
   tau_wheel = tau*ratio
   F_wheel = tau_wheel/rover['wheel']['radius']
   Fd = 6*F_wheel
