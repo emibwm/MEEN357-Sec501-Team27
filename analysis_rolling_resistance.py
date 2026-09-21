01d264a6481e8eeae4c2344bb656d730f153c4b4
Hailey Reyes <reyeshailey10@tamu.edu>
2026-09-21 15:25:33 -0500

    Update analysis_rolling_resistance.py

--- analysis_rolling_resistance.py
+++ analysis_rolling_resistance.py
@@ -63,7 +63,7 @@ for i in range(len(Crr_array)):
 
         omega = (omega_high + omega_low)/2
 
-        Ng = get_gear_ratio(rover['speed_reducer'])
+        Ng = get_gear_ratio(rover['wheel_assembly']['speed_reducer'])
         omega_wheel = omega / Ng
         v_max[i] = 0.30 * omega_wheel
 
